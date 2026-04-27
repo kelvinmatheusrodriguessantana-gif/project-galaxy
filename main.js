aa453eff740918714db9e8c1348ce3fc922808da
kelvinmatheusrodriguessantana-gif <kelvinmatheusrodriguessantana@gmail.com>
2026-04-26 23:16:41 -0300

    Create main.js file for Electron app with window configuration and IPC handlers

---
+++ main.js
@@ -0,0 +1,41 @@
+const { app, BrowserWindow, ipcMain } = require('electron');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        }
+    });
+
+    mainWindow.loadFile('index.html');
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
+
+// IPC Handlers
+ipcMain.on('some-event', (event, arg) => {
+    console.log(arg); // prints arg from renderer process
+    // You can send a response back
+    event.reply('some-reply', 'Response from main process');
+});
